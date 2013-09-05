d0b8ef32a65bea84a2273021a472fd1256eb61f6
zzxadi <zhazhexio@gmail.com>
2013-09-05 11:05:11 +0800

    update

---
+++ assets/themes/the-minimum/js/post.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(document).ready(function(e) {
+        window.disqus_shortname = 'zzx'; // required: replace example with your forum shortname
+    $('#disqus_container .comment').on('click',function(){
+        $(this).html('加载中...');
+        var that = this;
+        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
+    });
+});
